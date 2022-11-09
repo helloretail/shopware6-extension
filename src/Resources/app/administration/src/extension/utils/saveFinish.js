64d38aa093b61bf9cbf5fbd8348279f50291a570
Anders Gunder <ag@wexo.dk>
2022-11-09 08:49:17 +0100

    Eslint on saveFinish to fix bug with .map without return value

--- src/Resources/app/administration/src/extension/utils/saveFinish.js
+++ src/Resources/app/administration/src/extension/utils/saveFinish.js
@@ -1,6 +1,6 @@
 export default function saveFinish() {
     // Ensure saveFinish runs first, to allow vue to change page
-    this.$super("saveFinish");
+    this.$super('saveFinish');
 
     /* stop if not is Hello Retail channel */
     if (this.helloRetailService.getTypeId() !== this.salesChannel.typeId) {
@@ -16,22 +16,22 @@ export default function saveFinish() {
     }
 
     this.createNotificationInfo({
-        message: this.$tc("helret-hello-retail.save.info", 0, {
-            feedCount: Object.keys(feeds).length
-        })
+        message: this.$tc('helret-hello-retail.save.info', 0, {
+            feedCount: Object.keys(feeds).length,
+        }),
     });
 
     /* Generate feeds based on objects keys e.g. order and product */
-    Object.keys(feeds).map(feed => {
+    Object.keys(feeds).forEach(feed => {
         this.helloRetailService.generateFeed(salesChannelId, feed)
             .then(response => {
                 if (response.error) {
                     this.createNotificationError({
-                        message: response.message
+                        message: response.message,
                     });
                 } else {
                     this.createNotificationSuccess({
-                        message: response.message
+                        message: response.message,
                     });
                 }
             });
