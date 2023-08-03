f3803aa0c839a6ff834468b2a3466348c0f32d61
Kevin Valsted <kfyr@outlook.com>
2023-08-03 19:10:23 +0200

    add docker compose for shopware

---
+++ docker-config/shopware6/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo starting entrypoint
+sed -i "s/'https:\/\/d1pna5l3xsntoj.cloudfront.net\/scripts\/company\/awAddGift.js#{{ addWishPartnerId }}';/'https:\/\/d1pna5l3xsntoj.cloudfront.net\/scripts\/company\/awAddGift.js#{{ addWishPartnerId }},server_host=https:\/\/addwish.test,cdn_host=https:\/\/d1pna5l3xsntoj.cloudfront.test,core_host=https:\/\/core.helloretail.test,dashboard_host=https:\/\/my.helloretail.test';/g" /usr/app/src/custom/plugins/HelloRetail/src/Resources/views/storefront/component/hello-retail-tracking.html.twig
+exec $@
