b5b233f9a93463c8016ad2c98797cc68834f3a91
Thomas Holm Thomsen <thomas@wexo.dk>
2023-08-25 11:28:26 +0200

    Fix wrong formatting of routescope annotation

--- src/Core/Content/Export/SalesChannel/FileController.php
+++ src/Core/Content/Export/SalesChannel/FileController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(defaults: ['_routeScope' => 'storefront'])]
+#[Route(defaults: ['_routeScope' => ['storefront']])]
 class FileController extends AbstractController
 {
     public function __construct(
