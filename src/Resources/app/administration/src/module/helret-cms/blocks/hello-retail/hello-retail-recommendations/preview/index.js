a10d0dba731546c5df767a97909afc7ddbb1f74f
wexoigi <igi@wexo.dk>
2024-09-24 12:04:14 +0200

    use correct 6.6 assets function

--- src/Resources/app/administration/src/module/helret-cms/blocks/hello-retail/hello-retail-recommendations/preview/index.js
+++ src/Resources/app/administration/src/module/helret-cms/blocks/hello-retail/hello-retail-recommendations/preview/index.js
@@ -3,5 +3,11 @@ import template from './sw-cms-preview-hello-retail-recommendations.html.twig';
 const { Component } = Shopware;
 
 Component.register('sw-cms-preview-hello-retail-recommendations', {
-    template
+    template,
+
+    computed: {
+        assetFilter() {
+            return Shopware.Filter.getByName('asset');
+        },
+    }
 });
