22d3c7e92e1d751e80c77e7ca275d4fe7a8dc894
wexoigi <igi@wexo.dk>
2024-09-25 10:10:08 +0200

    set old recoms block in commerce category in shopping experience

--- src/Resources/app/administration/src/module/helret-cms/blocks/hello-retail/hello-retail-recommendations/preview/index.js
+++ src/Resources/app/administration/src/module/helret-cms/blocks/hello-retail/hello-retail-recommendations/preview/index.js
@@ -5,9 +5,4 @@ const { Component } = Shopware;
 Component.register('sw-cms-preview-hello-retail-recommendations', {
     template,
 
-    computed: {
-        assetFilter() {
-            return Shopware.Filter.getByName('asset');
-        },
-    }
 });
