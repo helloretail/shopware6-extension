6048d8d85256648ad90c1d0ebe31374d04ad8836
Kevin Valsted <kfyr@outlook.com>
2024-11-20 21:48:51 +0800

    switch to ecr

--- docker-config/build.sh
+++ docker-config/build.sh
@@ -4,7 +4,7 @@ TAG=$1
 
 mkdir -p exports
 docker cp shopware:/usr/app/src ./exports/shopware
-docker build -t ghcr.io/helloretail/shopware6-extension/shopware:$TAG -f docker-config/shopware6/Dockerfile .
+docker buildx build -t 029959346319.dkr.ecr.eu-west-1.amazonaws.com/devenv/shopware6/webserver:$TAG --platform "linux/amd64,linux/arm64" -f docker-config/shopware6/Dockerfile . --push
 docker cp shopwaredb:/var/lib/mysql ./exports/shopwaredb
-docker build -t ghcr.io/helloretail/shopware6-extension/shopwaredb:$TAG -f docker-config/shopwaredb/Dockerfile .
+docker buildx build -t 029959346319.dkr.ecr.eu-west-1.amazonaws.com/devenv/shopware6/db:$TAG --platform "linux/amd64,linux/arm64" -f docker-config/shopwaredb/Dockerfile . --push
 rm -r ./exports
