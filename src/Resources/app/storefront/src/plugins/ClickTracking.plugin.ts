b9650277b2f13ff7e00789dd992814e34301cad2
Anders Gunder <ag@wexo.dk>
2025-05-13 06:13:09 +0200

    Remove console.log

--- src/Resources/app/storefront/src/plugins/ClickTracking.plugin.ts
+++ src/Resources/app/storefront/src/plugins/ClickTracking.plugin.ts
@@ -11,7 +11,6 @@ export default class ClickTracking extends window.PluginBaseClass {
     this.awSource = this.el.dataset.awSource;
 
     const container = this.el.closest('.card,.search-suggest-product');
-    console.log(container);
     container?.querySelectorAll<HTMLAnchorElement>('a[href]').forEach((el) => {
       el.href = `${el.href}#aw_source=${this.awSource}`;
     });
