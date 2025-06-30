806daa300daf04a57fce7b7a7af49ee11c86c0b6
wexoigi <igi@wexo.dk>
2025-06-30 11:31:12 +0200

    log non-200 responses

--- src/Service/HelloRetailClientService.php
+++ src/Service/HelloRetailClientService.php
@@ -71,6 +71,15 @@ class HelloRetailClientService
             return [];
         }
 
+        if ($response->getStatusCode() !== 200) {
+            $this->logger->warning('HR API non-200 response', [
+                'endpoint' => $endpoint,
+                'status' => $response->getStatusCode(),
+                'body' => $body,
+            ]);
+            return [];
+        }
+
         return json_decode($response->getBody()->getContents(), true);
     }
 
