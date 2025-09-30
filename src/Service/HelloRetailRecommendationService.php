c2a434ce2e2f587e1c0fa6cbeabe01a3361dac3f
Nicolai Kure <nk@wexo.dk>
2025-09-30 17:40:01 +0200

    [49045-49] Updated HR context to give the correct info based on page for 6.6

--- src/Service/HelloRetailRecommendationService.php
+++ src/Service/HelloRetailRecommendationService.php
@@ -2,13 +2,17 @@
 
 namespace Helret\HelloRetail\Service;
 
+use Helret\HelloRetail\Service\Models\Recommendation;
 use Helret\HelloRetail\Service\Models\RecommendationContext;
+use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
 use Shopware\Core\Content\Category\CategoryEntity;
+use Shopware\Core\Content\Category\SalesChannel\SalesChannelCategoryEntity;
 use Shopware\Core\Content\Cms\DataResolver\CriteriaCollection;
 use Shopware\Core\Content\Product\ProductDefinition;
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductCollection;
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity;
 use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Struct\ArrayEntity;
 use Shopware\Core\System\SalesChannel\Aggregate\SalesChannelDomain\SalesChannelDomainEntity;
@@ -16,10 +20,12 @@ use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepository;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Core\Content\Product\ProductCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class HelloRetailRecommendationService
 {
     private const EXTRA_DATA = "extraData";
+    private const TRACKING_CODE = "trackingCode";
     private const ENDPOINT = "recoms";
 
     /**
@@ -27,7 +33,10 @@ class HelloRetailRecommendationService
      */
     public function __construct(
         protected readonly HelloRetailClientService $client,
-        protected readonly SalesChannelRepository $salesChannelRepository
+        protected readonly SalesChannelRepository $salesChannelRepository,
+        protected readonly entityRepository $productRepository,
+        protected readonly CartService $cartService,
+        protected RequestStack $requestStack
     ) {
     }
 
@@ -41,7 +50,7 @@ class HelloRetailRecommendationService
         $hierarchies = [];
         $urls = [];
         $category = null;
-        if ($entity::class == CategoryEntity::class) {
+        if ($entity::class == CategoryEntity::class || $entity::class == SalesChannelCategoryEntity::class) {
             $category = $entity;
         } elseif ($entity::class == SalesChannelProductEntity::class) {
             $category = $entity->getSeoCategory();
@@ -58,7 +67,7 @@ class HelloRetailRecommendationService
 
         $productData = $this->fetchRecommendations(
             $key,
-            $salesChannelContext->getSalesChannelId(),
+            $salesChannelContext,
             [$hierarchies],
             $urls
         );
@@ -80,20 +89,64 @@ class HelloRetailRecommendationService
 
     public function getRecommendations(string $key, SalesChannelContext $context): ?EntityCollection
     {
-        $productData = $this->fetchRecommendations($key, $context->getSalesChannelId());
+        $productData = $this->fetchRecommendations($key, $context);
         return $this->getProducts($productData, $context);
     }
 
     private function fetchRecommendations(
         string $key,
-        string $salesChannelId,
+        SalesChannelContext $salesChannelContext,
         array $hierarchies = [],
-               $urls = []
+        $urls = []
     ): array {
+        $salesChannelId = $salesChannelContext->getSalesChannelId();
+        $currentRequest = $this->requestStack->getCurrentRequest();
+        $route = $currentRequest->attributes->get('_route');
+        $requestUri = $currentRequest->attributes->get('sw-original-request-uri');
+
+        if (!empty($hierarchies[0])) {
+            $brand = $hierarchies[0][count($hierarchies[0]) - 1];
+        } else {
+            $brand = "";
+        }
+
+        $fullUrls = [];
+        foreach ($urls as $url) {
+            $fullUrls[] = $url . $requestUri;
+        }
+
+        if ($route == 'frontend.checkout.cart.page' || $route == 'frontend.cart.offcanvas') {
+            foreach ($salesChannelContext->getSalesChannel()->getDomains() as $domain) {
+                $urls[] = $domain->getUrl();
+            }
+        }
+
         if ($key) {
             $productData = [];
-            $context = new RecommendationContext($hierarchies, "", $urls);
-            $request = new Models\Recommendation($key, [self::EXTRA_DATA], $context);
+            $context = new RecommendationContext();
+            switch ($route) {
+                case 'frontend.home.page': // Home page
+                    break;
+                case 'frontend.navigation.page': // Category page
+                    $context->setHierarchies($hierarchies);
+                    $context->setBrand($brand);
+                    break;
+                case 'frontend.detail.page': // Product page
+                    $context->setUrls($fullUrls);
+                    break;
+                case 'frontend.cart.offcanvas': // Cart offcanvas
+                    $cartUrls = $this->getCartUrls($salesChannelContext, $urls);
+                    $context->setUrls($cartUrls);
+                    break;
+                case 'frontend.checkout.cart.page': // Cart page
+                    $cartUrls = $this->getCartUrls($salesChannelContext, $urls);
+                    $context->setUrls($cartUrls);
+                    break;
+                default:
+                    break;
+            }
+
+            $request = new Recommendation($key, [self::EXTRA_DATA, self::TRACKING_CODE], $context);
             $callback = $this->client->callApi(
                 endpoint: self::ENDPOINT,
                 request: $request,
@@ -141,5 +194,41 @@ class HelloRetailRecommendationService
         }
 
         return $ids;
+    }
+
+    private function getCartUrls(SalesChannelContext $salesChannelContext, array $urls) : array
+    {
+        $cartUrls = [];
+        $productIds = [];
+        $cart = $this->cartService->getCart($salesChannelContext->getToken(), $salesChannelContext);
+        foreach ($cart->getLineItems() as $lineItem) {
+            $productIds[] = $lineItem->getId();
+        }
+
+        $productSeo =$this->getProductSEO($productIds, $salesChannelContext->getContext());
+
+        foreach ($urls as $baseUrl) {
+            foreach ($productSeo as $productId => $seoUrl) {
+                $cartUrls[] = $baseUrl . '/' . $seoUrl;
+            }
+        }
+        return $cartUrls;
+    }
+
+    private function getProductSEO(array $productIds, $context): array
+    {
+        $productSeo = [];
+
+        $criteria = new Criteria($productIds);
+        $criteria->addAssociation('seoUrls');
+
+        /** @var ProductCollection|null $products */
+        $products = $this->productRepository->search($criteria, $context)->getEntities();
+
+        foreach ($products as $product) {
+            $productSeo[$product->getId()] = $product->getSeoUrls()->first()->getSeoPathInfo();
+        }
+
+        return $productSeo;
     }
 }
